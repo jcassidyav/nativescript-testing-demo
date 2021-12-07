1e965f880bd4cf95873de799ed8829e42ba56d3a
Jason Cassidy <jcassidy@avaeon.com>
2021-12-07 11:35:52 +0000

    add test runner 3

---
+++ src/main.spec.ts
@@ -0,0 +1,11 @@
+import './polyfills';
+import 'zone.js/dist/zone-testing.js';
+import { TestBed } from '@angular/core/testing';
+import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { NativeScriptTestingModule } from '@nativescript/angular/testing';
+
+TestBed.initTestEnvironment(
+  NativeScriptTestingModule,
+  platformBrowserDynamicTesting(),
+  { teardown: {destroyAfterEach: true}}
+);
